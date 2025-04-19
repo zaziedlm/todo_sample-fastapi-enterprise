7a20adf216b67c0a50dae193c9119a913dc165c1
Shuichi Kataoka <shu01k9@gmail.com>
2025-04-19 09:19:19 +0900

    E501 line too long (81 > 79 characters)

--- migrations/env.py
+++ migrations/env.py
@@ -37,7 +37,8 @@ def run_migrations_online():
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(connection=connection,
+                          target_metadata=target_metadata)
         with context.begin_transaction():
             context.run_migrations()
 
