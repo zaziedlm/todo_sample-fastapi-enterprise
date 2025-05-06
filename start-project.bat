95dc77edae1b52b5a17e0a9a5f41aa79a3ab677a
Shuichi Kataoka <shu01k9@gmail.com>
2025-05-06 16:23:38 +0900

    app execute(front,back-end) bat command.

---
+++ start-project.bat
@@ -0,0 +1,3 @@
+@echo off
+start powershell -NoExit -Command "cd '%~dp0'; python -m uvicorn app.main:app --reload --port 8000"
+start powershell -NoExit -Command "cd '%~dp0todo-frontend'; npm run dev"
