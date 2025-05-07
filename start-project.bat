462faa0fc07663064b8a6ee81eff888a2c9d027b
Shuichi Kataoka <shu01k9@gmail.com>
2025-05-07 12:56:06 +0900

    依存モジュールの取り込み、最新化 PowerShell起動を pwsh へ変更

--- start-project.bat
+++ start-project.bat
@@ -1,3 +1,3 @@
 @echo off
-start powershell -NoExit -Command "cd '%~dp0'; python -m uvicorn app.main:app --reload --port 8000"
-start powershell -NoExit -Command "cd '%~dp0todo-frontend'; npm run dev"
+start pwsh -NoExit -Command "cd '%~dp0'; python -m uvicorn app.main:app --reload --port 8000"
+start pwsh -NoExit -Command "cd '%~dp0todo-frontend'; npm run dev"
