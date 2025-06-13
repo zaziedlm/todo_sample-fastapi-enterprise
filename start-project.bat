8ce223f010682e8b6c964c0d0356d929ac8c90c0
Shuichi Kataoka <shu01k9@gmail.com>
2025-06-13 14:44:50 +0900

    backend起動に、uv run uvicorn を使うように変更

--- start-project.bat
+++ start-project.bat
@@ -1,3 +1,3 @@
 @echo off
-start pwsh -NoExit -Command "cd '%~dp0'; python -m uvicorn app.main:app --reload --port 8000"
+start pwsh -NoExit -Command "cd '%~dp0'; uv run uvicorn app.main:app --reload --host 0.0.0.0 --port 8000"
 start pwsh -NoExit -Command "cd '%~dp0todo-frontend'; npm run dev"
